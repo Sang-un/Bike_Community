9e2bc5466a3ee212983dfd62c9259057d94d15b4
kingchan223 <kokiyo97@naver.com>
2022-01-26 17:16:01 +0900

    로그인 서비스 Demo push

--- community/src/main/java/bike/community/CommunityApplication.java
+++ community/src/main/java/bike/community/CommunityApplication.java
@@ -2,11 +2,27 @@ package bike.community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CommunityApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {return new BCryptPasswordEncoder();}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:8081");
+			}
+		};
 	}
 }
