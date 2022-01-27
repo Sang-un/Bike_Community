44c000a430f5734c316d615623c64db282102734
kingchan223 <kokiyo97@naver.com>
2022-01-27 19:55:22 +0900

    Create UserControllerTest.java

---
+++ community/src/test/java/bike/community/controller/user/UserControllerTest.java
@@ -0,0 +1,35 @@
+package bike.community.controller.user;
+
+import bike.community.model.network.Header;
+import bike.community.model.network.request.user.JoinUserRequest;
+import bike.community.model.network.response.post.user.AfterJoinUserResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+class UserControllerTest {
+
+    @Autowired
+    private UserController userController;
+
+    @Test
+    void join() {
+        JoinUserRequest joinUserRequest = JoinUserRequest.builder()
+                .email("kokiyo9@naver.com")
+                .birthday("970223")
+                .nickname("킹찬영짱짱맨")
+                .password("1111")
+                .phone("01075528034")
+                .username("이찬영")
+                .sex("남")
+                .build();
+
+        Header<AfterJoinUserResponse> join = userController.join(joinUserRequest);
+        System.out.println(join);
+    }
+}
