af1399a1c09fb5ad11cf8cf80e94efad8f95aadf
kingchan223 <kokiyo97@naver.com>
2022-01-28 09:17:17 +0900

    merge into login-service branch test

--- community/src/main/java/bike/community/controller/user/UserController.java
+++ community/src/main/java/bike/community/controller/user/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-
+//merge into login-service branch test
 @Slf4j
 @RequiredArgsConstructor
 @RestController
