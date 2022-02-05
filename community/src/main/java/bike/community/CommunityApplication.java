65a61a158960dc1be59f7d5bc32456a614d84b1c
kingchan223 <kokiyo97@naver.com>
2022-02-05 22:06:42 +0900

    Log aop 적용하기

--- community/src/main/java/bike/community/CommunityApplication.java
+++ community/src/main/java/bike/community/CommunityApplication.java
@@ -1,5 +1,8 @@
 package bike.community;
 
+import bike.community.aop.log.LogTrace;
+import bike.community.aop.log.LogTraceAspect;
+import bike.community.aop.log.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -17,5 +20,15 @@ public class CommunityApplication {
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public LogTrace logTrace(){
+		return new ThreadLocalLogTrace();
+	}
+
+	@Bean
+	public LogTraceAspect logTraceAspect(LogTrace logTrace){
+		return new LogTraceAspect(logTrace);
 	}
 }
--- community/src/main/java/bike/community/model/user/User.java
+++ community/src/main/java/bike/community/model/user/User.java
@@ -25,18 +25,11 @@ public class User extends DateBaseEntity implements Serializable {
     private String nickname; //사용할 닉네임
 
     @Enumerated(EnumType.STRING)
-    private UserRole role;
+    private UserRole role;//가입 회원은 USER, 가입 안한 회원은 GUEST
 
+    //for oauth
     private String provider;
     private String providerId;
-
-//    private String email; //이메일
-//    private String password; //패스워드
-//    private String username; //본명 (진짜 본인 이름)
-//    private String sex; //성별
-//    private String phone; //전화번호 ('-'없이 ex:01075528034)
-//    private String birthday; //생년월일 7자리 (ex: 970223)
-//    private String nickname; //사용할 닉네임
 
     public static User makeUser(String email, String password, String username, String sex, String phone, String birthday, String nickname) {
         User user = new User();
@@ -51,7 +44,7 @@ public class User extends DateBaseEntity implements Serializable {
         return user;
     }
 
-
+    //for test
     public User(String email, String nickname) {
         this.email = email;
         this.nickname = nickname;
